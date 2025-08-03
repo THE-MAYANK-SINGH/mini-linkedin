c6bfd810c04f573c68691fa8b8f5a66ef18112e7
MAYANK SINGH <67534251+THE-MAYANK-SINGH@users.noreply.github.com>
2025-08-03 14:49:59 +0530

    Update page.js

--- client/app/page.js
+++ client/app/page.js
@@ -36,7 +36,7 @@ export default function HomePage() {
     setError("");
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/posts`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
